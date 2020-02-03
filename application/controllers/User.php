4c45de3c7dfc39cd9b4ffb3b114b118f278d80f6
naufalriefyan <58657931+naufalriefyan@users.noreply.github.com>
2020-02-03 20:40:09 +0700

    edit function user_profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -14,22 +14,8 @@ class User extends CI_Controller
 
     public function User_profile()
     {
-        $url = "https://randomuser.me/api/";
-        $get_url = file_get_contents($url);
-        $data = json_decode($get_url, true);
-        $data_array = [
-            'name' => $data['results'][0]['name']['first'],
-            'gender' => $data['results'][0]['gender'],
-            'address' => $data['results'][0]['location']['city'],
-            'state' => $data['results'][0]['location']['state'],
-            'country' => $data['results'][0]['location']['country'],
-            'email' => $data['results'][0]['email'],
-            'phone' => $data['results'][0]['phone'],
-            'picture' => $data['results'][0]['picture']['large']
-        ];
-
         $this->load->view('template/header');
-        $this->load->view('Data/profile', $data_array);
+        $this->load->view('Data/profile');
         $this->load->view('template/footer');
     }
 }
